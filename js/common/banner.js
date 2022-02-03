375760b9ce1176d6578e8110b8cf8ae7e68fd495
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-02-03 22:26:40 +0100

    Banner has logo and background image from the API

---
+++ js/common/banner.js
@@ -0,0 +1,16 @@
+import { baseUrl } from "../components/api.js";
+const banner = document.querySelector(".banner");
+
+
+const homeUrl = baseUrl + "/home";
+
+(async function bannerHero() {
+    try {
+        const response = await fetch(homeUrl);
+        const json = await response.json();
+        console.log(baseUrl + json.hero_banner.url);
+        banner.style.backgroundImage = `url("${baseUrl}${json.hero_banner.formats.medium.url}")`;
+    } catch (error) {
+        console.log(error);
+    }
+})();
