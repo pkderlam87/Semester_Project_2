01db5e3a0df1cc7345eeb14b6c0b8cbf71de817a
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-02-24 10:18:45 +0100

    The product.js have a function to manage the images.

--- js/util/products.js
+++ js/util/products.js
@@ -2,16 +2,16 @@ import { baseUrl } from "../components/api.js";
 import { getToken } from "../components/saveTokenAndUser.js";
 import { deleteProduct } from "./login/authResources/deleteProduct.js";
 
+
 export function showProducts(json) {
     const products = document.querySelector(".products");
     products.innerHTML = "";
     for (let i = 0; i < json.length; i++) {
-        if (json[i].image_url === null || json[i].image_url.length === 0) {
-            products.innerHTML += `
+        products.innerHTML += `
         <div>
         <a class = "product" id="product" href = "/product_detail.html?id=${json[i].id}">
         <div class="card" style="width: 18rem;">
-            <img src="${baseUrl}${json[i].image.formats.small.url}" class="card-img-top" alt="${json[i].image.alternativeText}">
+            <div class="image__place"></div>
             <div class="card-body">
                 <h5 class="card-title">${json[i].title}</h5>
                 <h5 class="card-text">${json[i].price} Nok</h5>
@@ -20,23 +20,10 @@ export function showProducts(json) {
                 </div>
             </div>
             </div></a></div>`;
-        } else {
-            products.innerHTML += `
-        <div>
-        <a class = "product" id="product" href = "/product_detail.html?id=${json[i].id}">
-        <div class="card" style="width: 18rem;">
-            <img src="${json[i].image_url}" class="card-img-top" alt="${json[i].title}">
-            <div class="card-body">
-                <h5 class="card-title">${json[i].title}</h5>
-                <h5 class="card-text">${json[i].price} Nok</h5>
-                <div class = "button__card">
-                <a href="/product_detail.html?id=${json[i].id}" class="btn btn-primary">View product</a>
-                </div>
-            </div>
-            </div></a></div>`;
-        }
-
     }
+    const imagePlace = document.querySelectorAll(".image__place");
+    showImageCard(imagePlace, json);
+
     const token = getToken();
     if (token) {
         const buttonCard = document.querySelectorAll(".button__card");
@@ -44,7 +31,6 @@ export function showProducts(json) {
     }
 }
 function changeButton(buttonCard, json) {
-    console.log(json);
     for (let i = 0; i < buttonCard.length; i++) {
         buttonCard[i].innerHTML = `<div class="container justify-content-between"><i class="ri-delete-bin-2-fill btn btn-delete" data-id=${json[i].id}></i> <a href="/edit.html?id=${json[i].id}"><i class="ri-edit-line btn"></i></a></div>`;
     };
@@ -52,4 +38,14 @@ function changeButton(buttonCard, json) {
     deleteButton.forEach(element => {
         element.addEventListener("click", deleteProduct);
     });
+}
+
+function showImageCard(imagePlace, json) {
+    for (let i = 0; i < imagePlace.length; i++) {
+        if (json[i].image_url === null || json[i].image_url.length === 0) {
+            imagePlace[i].innerHTML += `<img src="${baseUrl}${json[i].image.formats.small.url}" class="card-img-top" alt="${json[i].image.alternativeText}"></img>`
+        } else {
+            imagePlace[i].innerHTML += `<img src="${json[i].image_url}" class="card-img-top" alt="${json[i].title}">`;
+        }
+    }
 }
